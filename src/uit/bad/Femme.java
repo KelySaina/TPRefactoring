cf4f3e4c70f82cf38ad2efaba0c030dc3bfdf36a
KelySaina <kelysaina@gmail.com>
2024-03-28 08:40:14 +0300

    Class Femme

---
+++ src/uit/bad/Femme.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uit.bad;
+
+/**
+ *
+ * @author thyler
+ */
+public class Femme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme() {
+    }
+    
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}
