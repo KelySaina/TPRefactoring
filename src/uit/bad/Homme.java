f7dff1c21a4ca412a1b3b063b5f3c240e87f42c8
KelySaina <kelysaina@gmail.com>
2024-03-28 08:39:02 +0300

    Package + Class Homme

---
+++ src/uit/bad/Homme.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uit.bad;
+
+/**
+ *
+ * @author thyler
+ */
+public class Homme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Homme() {
+    }
+    
+
+    public Homme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    
+}
